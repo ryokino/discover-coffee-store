771a60ae9dfeb24a7a03fd813743569d717c73a2
ryokino <ryo.kinoshita212@gmail.com>
2023-03-01 21:25:43 +0900

    font changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+// Document をextendsするためにfunctionを使っていない
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            rel='preload'
+            href='/fonts/Pacifico-Regular.ttf'
+            as='font'
+            crossOrigin='anonymous'></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
